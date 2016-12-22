ee389019616237de1a16a0ecad13fcc4d2ac2d35
unknown <>
2016-12-22 21:15:55 +0900

    Turtle draws circle

--- turtle/exam1.py
+++ turtle/exam1.py
@@ -1,7 +1,14 @@
 import turtle
 
-turtle.forward(40)
-turtle.left(30)
-turtle.forward(40)
-turtle.left(60)
-turtle.forward(30)
+turtle.shape("turtle")
+original_xcor = turtle.xcor()
+original_ycor = turtle.ycor()
+speed = 2
+while True:
+    turtle.forward(speed)
+    turtle.left(10)
+    speed += 0.1
+    if turtle.distance(original_xcor, original_ycor) > 90:
+        break
+
+turtle.exitonclick()
